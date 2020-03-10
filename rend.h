6f6310c8a20bc6a5a39cf615e9d1c62ed545d137
Nikaoto <nikaoto@gmail.com>
2020-03-10 22:52:48 +0400

    make logvec fns pointer compatible

--- rend.h
+++ rend.h
@@ -3,8 +3,9 @@
 
 #define logf(f) printf(#f " = %g\n", f);
 #define logi(i) printf(#i " = %i\n", i);
-#define logvec(v) printf(#v " = (%g, %g, %g)\n", v.x, v.y, v.z);
-#define logvec2i(v) printf(#v " = (%i, %i)\n", v.x, v.y);
+#define logvec(v) printf(#v " = (%g, %g, %g)\n", (v).x, (v).y, (v).z);
+#define logvec2(v) printf(#v " = (%g, %g)\n", (v).x, (v).y);
+#define logvec2i(v) printf(#v " = (%i, %i)\n", (v).x, (v).y);
 
 #define WIREFRAME      1
 #define WINDOW_WIDTH   1024
